df4a49d1f111d9b07f73aefa617c273afee06a95
AzusaChino <azusa146@gmail.com>
2020-03-17 13:56:46 +0800

    ft. func

--- src/tree/tree-main.go
+++ src/tree/tree-main.go
@@ -1,6 +1,26 @@
 package tree
 
+import (
+	"fmt"
+)
+
+func (node *Node) Traverse() {
+	node.TraverseFunc(func(node *Node) {
+		node.print()
+	})
+	fmt.Println("")
+}
+
+func (node *Node) TraverseFunc(f func(*Node)) {
+	if node == nil {
+		return
+	}
+	node.Left.TraverseFunc(f)
+	f(node)
+	node.Right.TraverseFunc(f)
+}
+
 func main() {
 	var root Node
-	print(root)
+	root.Traverse()
 }
