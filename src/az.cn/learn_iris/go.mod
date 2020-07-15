9546c1f10d3c34eb985806c67db328641aaa6fbf
AzusaChino <azusa146@gmail.com>
2020-07-15 23:55:55 +0800

    ft. change file locations & add go.mod files

---
+++ src/az.cn/learn_iris/go.mod
@@ -0,0 +1,8 @@
+module az.cn/learn_iris
+
+go 1.14
+
+require (
+    github.com/kataras/iris/v12 v12.1.8
+)
+
--- src/learn_project/code/queue/main.go
+++
@@ -1,9 +0,0 @@
-package queue
-
-import "fmt"
-
-func main() {
-	var q Queue
-	q.Push("water")
-	fmt.Println(q.Pop())
-}
