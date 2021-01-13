ac957a0686ba720c8a40892dd2dfede66b0deb36
ycpang <ycpang@iflytek.com>
2021-01-13 11:25:21 +0800

    Go中的MapReduce和代码泛型&生成

---
+++ src/practices/map_reduce/generic.go
@@ -0,0 +1,38 @@
+package map_reduce
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// 泛型MapReduce ==> 编译通过，运行时报错
+
+// 通过 reflect.ValueOf() 来获得 interface{} 的值，其中一个是数据 vdata，另一个是函数 vfn，
+// 然后通过 vfn.Call() 方法来调用函数，通过 []reflect.Value{vdata.Index(i)}来获得数据。
+func Map(data interface{}, fn interface{}) []interface{} {
+	vfn := reflect.ValueOf(fn)
+	vdata := reflect.ValueOf(data)
+	result := make([]interface{}, vdata.Len())
+	for i := 0; i < vdata.Len(); i++ {
+		result[i] = vfn.Call([]reflect.Value{vdata.Index(i)})[0].Interface()
+	}
+	return result
+}
+
+func main() {
+	square := func(x int) int {
+		return x * x
+	}
+	nums := []int{1, 2, 3, 4}
+	squared_arr := Map(nums, square)
+	fmt.Println(squared_arr)
+	//[1 4 9 16]
+	upcase := func(s string) string {
+		return strings.ToUpper(s)
+	}
+	strs := []string{"Hao", "Chen", "MegaEase"}
+	upstrs := Map(strs, upcase)
+	fmt.Println(upstrs)
+	//[HAO CHEN MEGAEASE]
+}
---
+++ src/practices/map_reduce/map.go
@@ -0,0 +1,68 @@
+package map_reduce
+
+import (
+	"fmt"
+	"strings"
+)
+
+func MapStrToStr(arr []string, fn func(string) string) []string {
+	var newArray = []string{}
+	for _, it := range arr {
+		newArray = append(newArray, fn(it))
+	}
+	return newArray
+}
+
+func MapStrToInt(arr []string, fn func(s string) int) []int {
+	var newArray = []int{}
+	for _, it := range arr {
+		newArray = append(newArray, fn(it))
+	}
+	return newArray
+}
+
+func Reduce(arr []string, fn func(string) int) int {
+	sum := 0
+	for _, it := range arr {
+		sum += fn(it)
+	}
+	return sum
+}
+
+func Filter(arr []int, fn func(int) bool) []int {
+	var newArray = []int{}
+	for _, it := range arr {
+		if fn(it) {
+			newArray = append(newArray, it)
+		}
+	}
+	return newArray
+}
+
+func main() {
+	var list = []string{"Hao", "Chen", "MegaEase"}
+	x := MapStrToStr(list, func(s string) string {
+		return strings.ToUpper(s)
+	})
+	fmt.Printf("%v\n", x)
+	//["HAO", "CHEN", "MEGAEASE"]
+	y := MapStrToInt(list, func(s string) int {
+		return len(s)
+	})
+	fmt.Printf("%v\n", y)
+	//[3, 4, 8]
+	z := Reduce(list, func(s string) int {
+		return len(s)
+	})
+	fmt.Printf("%v\n", z)
+
+	var intset = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	out := Filter(intset, func(n int) bool {
+		return n%2 == 1
+	})
+	fmt.Printf("%v\n", out)
+	out = Filter(intset, func(n int) bool {
+		return n > 5
+	})
+	fmt.Printf("%v\n", out)
+}
