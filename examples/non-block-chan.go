cb3823ff0f9c41dd0c2d5589b15611801b3f63bc
AzusaChino <azusa146@gmail.com>
2021-02-05 21:28:51 +0800

    feat. add few more examples --> timers

---
+++ examples/non-block-chan.go
@@ -0,0 +1,32 @@
+package examples
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received msg:", msg)
+	default:
+		fmt.Println("no message")
+	}
+
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("send msg:", msg)
+	default:
+		fmt.Println("no msg sent")
+	}
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
